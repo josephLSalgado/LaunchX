c6087dc0580010e69ddc70106d2ad4759dad2691
josephLSalgado <josephLSalgado@outlook.com>
2022-03-25 13:50:58 -0600

    feat: Agregado componente de Eslogan

---
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/components/Eslogan.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Los recuerdos más dulces desde 1996</h1>
+</template>
+
+<script>
+export default {
+    name: 'Eslogan'
+}
+</script>
+
+<style scoped>
+h1 {
+    font-style: italic;
+    color: #9c7157;
+}
+</style>
--- FrontEnd/5-VUEJS/pasteleria-d-joseph/src/views/InicioView.vue
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/views/InicioView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="inicio">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Eslogan/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Eslogan from '@/components/Eslogan.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'InicioView',
   components: {
-    HelloWorld
+    Eslogan
   }
 }
 </script>
