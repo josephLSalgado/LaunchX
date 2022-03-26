01476ce8c6e0a7b45f5fdcbe778c6d70de8d8f5e
josephLSalgado <josephLSalgado@outlook.com>
2022-03-25 21:41:59 -0600

    feat: Agregado componente InformacionPasteleria.vue

---
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/components/InformacionPasteleria.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="informacion-pasteleria">
+        <table>
+            <tr class="encabezados">
+                <th>Sucursal</th>
+                <th>Dirección</th>
+                <th>Teléfono</th>
+                <th>Horarios</th>
+                <th>Síguenos</th>
+            </tr>
+            <tr class="datos">
+                <td>{{ $store.getters.obtenerSucursal }}</td>
+                <td>{{ $store.getters.obtenerDireccion }}</td>
+                <td>{{ $store.getters.obtenerTelefono }}</td>
+                <td>{{ $store.getters.obtenerHorarios }}</td>
+                <td class="redes">
+                    <img src="../assets/facebook.png" alt="Facebook">
+                    <img src="../assets/gorjeo.png" alt="Twitter">
+                    <img src="../assets/youtube.png" alt="Youtube">
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InformacionPasteleria'
+}
+</script>
+
+<style scoped>
+.informacion-pasteleria {
+    background-color: #a17f6a;
+}
+
+table {
+    margin-right: auto;
+    margin-left: auto;
+}
+
+.encabezados th {
+    font-weight: 900;
+}
+
+.datos td {
+    padding: 10px;
+}
+
+.redes img {
+    padding-right: 5px;
+    padding-left: 5px;
+}
+</style>
--- FrontEnd/5-VUEJS/pasteleria-d-joseph/src/store/index.js
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/store/index.js
@@ -2,8 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    sucursal: 'San Luis Potosí',
+    direccion: 'Av. Huasteca 295 Col. Lomas de San Luis Potosí Mpo San Luis Potosí CP 78210',
+    telefono: '444 833 6496',
+    horarios: 'Lun a Dom de 08:30 a 20:30 h'
   },
   getters: {
+    obtenerSucursal(state) {
+      return state.sucursal;
+    },
+    obtenerDireccion(state) {
+      return state.direccion;
+    },
+    obtenerTelefono(state) {
+      return state.telefono;
+    },
+    obtenerHorarios(state) {
+      return state.horarios;
+    }
   },
   mutations: {
   },
