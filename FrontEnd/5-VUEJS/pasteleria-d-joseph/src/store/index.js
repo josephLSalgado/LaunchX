c480349d247200640e319701ae320cf9070d95db
josephLSalgado <josephLSalgado@outlook.com>
2022-03-26 22:11:48 -0600

    feat: Agregado componente Pasteles.vue

--- FrontEnd/5-VUEJS/pasteleria-d-joseph/src/store/index.js
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/store/index.js
@@ -5,7 +5,29 @@ export default createStore({
     sucursal: 'San Luis Potosí',
     direccion: 'Av. Huasteca 295 Col. Lomas de San Luis Potosí Mpo San Luis Potosí CP 78210',
     telefono: '444 833 6496',
-    horarios: 'Lun a Dom de 08:30 a 20:30 h'
+    horarios: 'Lun a Dom de 08:30 a 20:30 h',
+    productos: {
+      0: {
+        nombre: 'Pastel Pingüino',
+        foto: 'https://cdn.shopify.com/s/files/1/0262/5080/5306/products/497982-1_600x.jpg?v=1618602708',
+        precio: '$418.00'
+      },
+      1: {
+        nombre: 'Salón intenso individual',
+        foto: 'https://cdn.shopify.com/s/files/1/0262/5080/5306/products/27167-1_600x.jpg?v=1618602784',
+        precio: '$42.00'
+      },
+      2: {
+        nombre: 'Pastel Durazno',
+        foto: 'https://cdn.shopify.com/s/files/1/0262/5080/5306/products/497131-1_600x.jpg?v=1618602666',
+        precio: '$618.00'
+      },
+      3: {
+        nombre: 'Pastel Baileys',
+        foto: 'https://cdn.shopify.com/s/files/1/0262/5080/5306/products/497440-1_600x.jpg?v=1620877558',
+        precio: '$518.00'
+      }
+    }
   },
   getters: {
     obtenerSucursal(state) {
@@ -19,6 +41,9 @@ export default createStore({
     },
     obtenerHorarios(state) {
       return state.horarios;
+    },
+    obtenerProductos(state) {
+      return state.productos;
     }
   },
   mutations: {
