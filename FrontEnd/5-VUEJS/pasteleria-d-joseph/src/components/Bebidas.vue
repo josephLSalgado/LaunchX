c9faa0a8b001d18d525b8eedb65816d242307af8
josephLSalgado <josephLSalgado@outlook.com>
2022-04-02 20:50:03 -0600

    feat: Agregado botón de ir directamente a la compra del producto

--- FrontEnd/5-VUEJS/pasteleria-d-joseph/src/components/Bebidas.vue
+++ FrontEnd/5-VUEJS/pasteleria-d-joseph/src/components/Bebidas.vue
@@ -5,9 +5,20 @@
             <!-- Renderizado de una lista con un objeto-->
             <li v-for="(producto, key, index) in $store.getters.obtenerBebidas"
                 v-bind:key="index">
-                <img v-bind:src="producto.foto" v-bind:alt="producto.nombre">
-                <p>{{ producto.nombre }}</p>
-                <span>{{ producto.precio }}</span>
+                <img v-bind:src="producto.foto" v-bind:alt="producto.nombre"
+                    class="imagen">
+                <div class="acomodo-detalles">
+                    <div class="detalles-producto">
+                        <p>{{ producto.nombre }}</p>
+                        <span>{{ producto.precio }}</span>
+                    </div>
+                    <div class="compra-producto">
+                        <button>
+                            <img src="@/assets/carro-de-la-carretilla.png" 
+                                alt="Carrito" class="compra">
+                        </button>
+                    </div>
+                </div>
             </li>
         </ul>
     </div>
@@ -43,9 +54,13 @@ li {
     padding-left: 10px;
 }
 
-.imagen li img{
+img.imagen{
     width: 250px;
     margin-top: 10px;
+}
+
+img.compra {
+    width: 16px;
 }
 
 .detalles li p {
@@ -56,4 +71,19 @@ span {
     font-weight: bold;
 }
 
+.acomodo-detalles {
+    display: inline-flex;
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+    width: 100%;
+}
+
+button {
+    border: 2px solid #9c7157;
+    background-color: white;
+    border-radius: 4px;
+    padding: 5px 20px;
+    cursor: pointer;
+}
 </style>
